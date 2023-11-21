fa0532376a192d4962fe5ba911545f248a415ef3
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-21 13:16:46 -0300

    Retornando projeto com funcionamento

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ async function buscarEMostrarVideos() {
       if (video.categoria == "") {
         throw new Error("Vídeo não tem categoria");
       }
-      containerVideos.innerHTML = `
-            <li class="videos__item">
-                <iframe> src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen><iframe>
-                <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="logo do canal">
-                    <h3 class="titulo-video">${video.titulo}</h3>
-                    <p class="titulo-canal">${video.descricao}</p>
-                </div>
-            </li>
-        `;
-    });
+      containerVideos.innerHTML += `
+                      <li class="videos__item">
+                      <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                      <div class="descricao-video">
+                          <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+                          <h3 class="titulo-video">${video.titulo}</h3>
+                          <p class="titulo-canal">${video.descricao}</p>
+                      </div>
+                  </li>
+                  `;
+    })
   } catch (error) {
     containerVideos.innerHTML = `
         <p>Houve um erro ao carregar os vídeos: ${error}</p>
-    `;
+    `
   }
 }
 
