66dc19730b310a86dd0fd7d56e6dd276877678ae
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-21 13:41:14 -0300

    titulo-video

--- script.js
+++ script.js
@@ -36,9 +36,20 @@ barraDePesquisa.addEventListener("input", filtrarPesquisa);
 function filtrarPesquisa() {
   const videos = document.querySelectorAll(".videos__item");
 
-  if(barraDePesquisa.value != "") {
+  if (barraDePesquisa.value != "") {
+    for (let video of videos) {
+      let titulo = video
+        .querySelector(".titulo-video")
+        .textContent.toLowerCase();
+      let valorFiltro = barraDePesquisa.value.toLowerCase();
 
+      if (!titulo.includes(valorFiltro)) {
+        video.style.display = "none";
+      } else {
+        video.style.display = "block";
+      }
+    }
   } else {
-    
+    video.style.display = "block";
   }
 }
